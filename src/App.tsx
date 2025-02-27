81e585f1dd16266f301c485d231d09521aa81300
ankitraminwar <ankitraminwar@gmail.com>
2025-02-27 06:22:04 +0000

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,26 @@ import Clock from "./components/clock";
 
 const App = () => {
   return (
-    <div className="clock-container">
-      <h1
-        key="quoteSting"
-        style={{ paddingBottom: "5px", borderBottom: "1px solid red" }}
+    <div>
+      <div className="clock-container">
+        <h1
+          key="quoteSting"
+          style={{ paddingBottom: "5px", borderBottom: "1px solid red" }}
+        >
+          The two most powerful warriors are patience and time...
+        </h1>
+        <Clock />
+      </div>
+      <div
+        style={{
+          float: "right",
+          margin: "5px",
+          marginTop: "25px",
+          opacity: 0.7,
+        }}
       >
-        The two most powerful warriors are patience and time...
-      </h1>
-      <Clock />
+        Developed by Ankit Raminwar
+      </div>
     </div>
   );
 };
